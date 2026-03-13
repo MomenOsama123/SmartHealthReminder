2010122b1ceba6f091b4c6d1a024ce650bcedf4b
rewan1234567890 <rewan.mohamedhafez12345@gmail.com>
2026-03-14 00:36:12 +0200

    Update MainActivity

--- app/src/main/java/com/smarthealthreminderwithaiassistant/MainActivity.kt
+++ app/src/main/java/com/smarthealthreminderwithaiassistant/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.verify_your_email)
+        setContentView(R.layout.create_new_password)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
