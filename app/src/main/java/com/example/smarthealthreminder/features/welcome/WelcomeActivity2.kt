6e457788ac7cdde138ad78c97b94d974c9f83d3b
mohamed_ali <midos123rrr@gmail.com>
2026-04-09 00:32:19 +0200

    linking between pages

--- app/src/main/java/com/example/smarthealthreminder/features/welcome/WelcomeActivity2.kt
+++ app/src/main/java/com/example/smarthealthreminder/features/welcome/WelcomeActivity2.kt
@@ -1,21 +1,37 @@
 package com.example.smarthealthreminder.features.welcome
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.smarthealthreminder.R
+import com.example.smarthealthreminder.databinding.ActivityWelcome2Binding
+import com.example.smarthealthreminder.features.auth.signin.SigninActivity
+import com.example.smarthealthreminder.features.auth.signup.SignupActivity
 
 class WelcomeActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityWelcome2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_welcome2)
+        binding = ActivityWelcome2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.signupBtn.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+        binding.loginTxt.setOnClickListener {
+            val intent = Intent(this, SigninActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
